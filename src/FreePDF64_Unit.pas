c1512e62a80e783c58690a4d97c09f1f91174048
Michael <38452654+FreePDF64@users.noreply.github.com>
2025-01-12 06:47:58 +0100

    Neue Version 6.1.2

--- src/FreePDF64_Unit.pas
+++ src/FreePDF64_Unit.pas
@@ -1886,15 +1886,15 @@ end;
 // Aufruf der Github-Release-Seite von FreePDF64
 procedure TFreePDF64_Form.AbfrageaufeinneuesUpdate1Click(Sender: TObject);
 var
-  DatumOhneZeit: string;
+  Datum: String;
 begin
-  DatumOhneZeit := FormatDateTime('dd.mm.yyyy', Now); // nur das Datum formatieren
+  Datum := FreePDF64_Form.LMDVersionInfo1.LastModified;
+  Delete(Datum, 11, 9);  // Entfernt die letzten 9 Zeichen
   ShowMessage('>>> Aktuelle Programminformationen <<<' + #13 + #13
               + LMDVersionInfo1.ProductName + ' Version '
-              + LMDVersionInfo1.ProductVersion + ' - 64 bit (' + DatumOhneZeit + ')');
+              + LMDVersionInfo1.ProductVersion + ' - 64 bit (' + Datum + ')');
   ShellExecute(Application.Handle, 'open', PChar('https://github.com/FreePDF64/FreePDF64/releases'), NIL, NIL, SW_NORMAL);
 end;
-
 // Wasserzeichen...
 procedure TFreePDF64_Form.Wasserzeichen1Click(Sender: TObject);
 begin
