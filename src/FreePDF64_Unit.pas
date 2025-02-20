cb1e0a5ae26ebde161928f01d87a309abd7f8f50
Michael <38452654+FreePDF64@users.noreply.github.com>
2025-02-20 08:08:35 +0100

    Neue Version 6.3.0

--- src/FreePDF64_Unit.pas
+++ src/FreePDF64_Unit.pas
@@ -2043,7 +2043,7 @@ procedure TFreePDF64_Form.AbfrageaufeinneuesUpdate1Click(Sender: TObject);
 var
   Datum: String;
 begin
-  Datum := '19.02.2025';
+  Datum := '20.02.2025';
   Delete(Datum, 11, 9); // Entfernt die letzten 9 Zeichen
   if MessageDlgCenter('Aktuell genutzt wird:' + ' Version ' + LMDVersionInfo1.ProductVersion + ' - 64 bit (' + Datum + ')' +
                    #13 + #13 + 'Mit Klick auf [ Ja ] geht es weiter zur FreePDF64-Releaseseite!', mtInformation, [mbYes, mbNo]) = mrYes then
