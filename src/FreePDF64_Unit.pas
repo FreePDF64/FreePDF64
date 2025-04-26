31877c57c766baafcc142817a5e10d8f0cd4c4d4
Michael <38452654+FreePDF64@users.noreply.github.com>
2025-04-26 07:11:27 +0200

    Neue Version 6.4.1

--- src/FreePDF64_Unit.pas
+++ src/FreePDF64_Unit.pas
@@ -2130,7 +2130,7 @@ procedure TFreePDF64_Form.AbfrageaufeinneuesUpdate1Click(Sender: TObject);
 var
   Datum: String;
 begin
-  Datum := '25.04.2025';
+  Datum := '26.04.2025';
   Delete(Datum, 11, 9); // Entfernt die letzten 9 Zeichen
   if MessageDlgCenter('Aktuell genutzt wird:' + ' Version ' +
     LMDVersionInfo1.ProductVersion + ' - 64 bit (' + Datum + ')' +
